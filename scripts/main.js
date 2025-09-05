d538297974c0c92576a2141ccc8dbce9464d8c59
Ducky Huang <ducky.huanggaiateche.com@DuckydeMacBook-Pro.local>
2025-09-05 22:00:08 +0800

    Day8-自動化部署

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg950\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 // \uc0\u21462 \u24471 \u25353 \u37397 \
+const button = document.getElementById("clickMeBtn");\
+\
+// \uc0\u35373 \u23450 \u20107 \u20214 \u30435 \u32893 \u22120 \
+button.addEventListener("click", () => \{\
+    alert("\uc0\u20320 \u21083 \u21083 \u40670 \u25802 \u20102 \u25353 \u37397 \u65281 Day8 Lab \u37096 \u32626 \u28204 \u35430 \u25104 \u21151  \u55356 \u57225 ");\
+\});\
+}
